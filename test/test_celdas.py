ed4e5e189452a9a5f22a6dee63ec10321186ce17
GabrielEberlein <gjeberlein@gmail.com>
2020-04-18 10:09:18 -0300

    Separo el test de la cantidad de celdas en Dos

--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,13 +1,19 @@
 from src.bingo import carton
 
-def test_cantidadDeCeldas():
+def contarCeldasOcupadas():
     _carton = carton()
     contador = 0
     for filas in _carton:
         for celdas in filas:
             contador+=celdas
 
-    assert contador == 15
+    return contador
+
+def test_masDe15Celdas():
+    assert not(contarCeldasOcupadas()>15)
+
+def test_menosDe15Celdas():
+    assert not(contarCeldasOcupadas()<15)
 
 def test_columnasVacias():
     _carton = carton()
