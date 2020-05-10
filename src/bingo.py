1fa5b4d3a8bab0d579049feeb70d6401d839e755
Gabriel <gjeberlein@gmail.com>
2020-05-09 23:21:22 -0300

    Muevo las funciones principales a bingo.py y dejo solo los asserts en test_celdas.py

--- src/bingo.py
+++ src/bingo.py
@@ -5,3 +5,32 @@ def carton():
     (1,0,1,0,1,0,1,0,1)
     )
     return carton
+
+def contarCeldasOcupadas(_carton):
+    contador = 0
+    for filas in _carton:
+        for celdas in filas:
+            if celdas > 0:
+                contador+=1
+
+    return contador
+
+def columnasOcupadas(_carton):
+    columnasValidas = 0
+    for indiceColumna in range(9):
+        if _carton[0][indiceColumna] or _carton[1][indiceColumna] or _carton[2][indiceColumna]:
+            columnasValidas+=1
+
+    return columnasValidas
+
+def filasOcupadas(_carton):
+    filasValidas = 0
+    for filas in _carton:
+        for celdas in filas:
+            if celdas:
+                filasValidas+=1
+                break
+    
+    return filasValidas
+
+
--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,36 +1,16 @@
-from src.bingo import carton
+from src import bingo
 
-def contarCeldasOcupadas():
-    _carton = carton()
-    contador = 0
-    for filas in _carton:
-        for celdas in filas:
-            contador+=celdas
+_carton = bingo.carton()
 
-    return contador
 
 def test_masDe15Celdas():
-    assert not(contarCeldasOcupadas()>15)
+    assert not(bingo.contarCeldasOcupadas(_carton)>15)
 
 def test_menosDe15Celdas():
-    assert not(contarCeldasOcupadas()<15)
+    assert not(bingo.contarCeldasOcupadas(_carton)<15)
 
-def test_columnasVacias():
-    _carton = carton()
-    columnasValidas = 0
-    for indiceColumna in range(9):
-        if _carton[0][indiceColumna] or _carton[1][indiceColumna] or _carton[2][indiceColumna]:
-            columnasValidas+=1
+def test_columnasOcupadas():
+    assert bingo.columnasOcupadas(_carton) == 9
 
-    assert columnasValidas == 9
-
-def test_filasVacias():
-    _carton = carton()
-    filasValidas = 0
-    for filas in _carton:
-        for celdas in filas:
-            if celdas:
-                filasValidas+=1
-                break
-    
-    assert filasValidas == 3
+def test_filasOcupadas():
+    assert bingo.filasOcupadas(_carton) == 3
