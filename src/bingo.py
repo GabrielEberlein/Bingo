5d066bf0e2e6470d1cd0fdf55d9f0e5107e111ed
Gabriel <gjeberlein@gmail.com>
2020-07-04 05:22:08 -0300

    Agrego la tracalada de tests que me faltaban

--- src/bingo.py
+++ src/bingo.py
@@ -1,20 +1,20 @@
 def carton():
     carton=(
-    (1,0,2,0,3,0,4,5,6),
-    (0,7,0,8,0,9,0,10,0),
-    (11,0,12,0,13,0,14,0,15)
+    (0,0,27,36,0,52,62,0,80),
+    (0,11,0,37,41,0,0,75,88),
+    (3,12,0,0,48,0,65,76,0)
     )
     return carton
 
 
-def contarCeldasOcupadas(_carton):
+def contarCeldasOcupadas(arr):
     contador = 0
-    for filas in _carton:
-        for celdas in filas:
-            if celdas > 0:
-                contador+=1
+    for celdas in arr:
+        if celdas > 0:
+            contador+=1
 
     return contador
+
 
 def columnasOcupadas(_carton):
     columnasValidas = 0
@@ -24,15 +24,12 @@ def columnasOcupadas(_carton):
 
     return columnasValidas
 
-def filasOcupadas(_carton):
-    filasValidas = 0
-    for filas in _carton:
-        for celdas in filas:
-            if celdas:
-                filasValidas+=1
-                break
-    
-    return filasValidas
+def menosDe3CeldasPorColumna(_carton):
+    columnasInvalidas=0
+    for columnas in range(9):
+        if(_carton[0][columnas] & _carton[1][columnas] & _carton[2][columnas]):
+            columnasInvalidas+=1
+    return columnasInvalidas
 
 def celdaInferior(_carton):
     celdasValidas=9
@@ -74,3 +71,23 @@ def valoresValidos(_carton):
                 celdasValidas+=1
 
     return celdasValidas
+
+def celdasOcupadasConsecutivas(_carton):
+    celdasConsecutivas = 0
+    for filas in range(3):
+        fila = _carton[filas]
+        for celda in range(7):
+            if fila[celda] and fila[celda+1] and fila[celda+2]:
+                celdasConsecutivas+=1
+    
+    return celdasConsecutivas
+
+def celdasVaciasConsecutivas(_carton):
+    celdasConsecutivas = 0
+    for filas in range(3):
+        fila = _carton[filas]
+        for celda in range(7):
+            if not(fila[celda] or fila[celda+1] or fila[celda+2]):
+                celdasConsecutivas+=1
+    
+    return celdasConsecutivas
--- test/test_celdas.py
+++ test/test_celdas.py
@@ -3,17 +3,32 @@ from src import bingo
 _carton = bingo.carton()
 
 
-def test_masDe15Celdas():
-    assert not(bingo.contarCeldasOcupadas(_carton)>15)
+def test_15celdasOcupadas():
+    celdasOcupadas = 0
+    for filas in _carton:
+        celdasOcupadas += bingo.contarCeldasOcupadas(filas) 
+    assert celdasOcupadas==15
 
-def test_menosDe15Celdas():
-    assert not(bingo.contarCeldasOcupadas(_carton)<15)
+def test_cartonDe3x9():
+    if(len(_carton[0])==len(_carton[1]) and len(_carton[1])==len(_carton[2])):
+        assert len(_carton) * len(_carton[0]) == 27
+    else:
+        assert False
+
+def test_columnasCon1CeldaOcupada():
+    columnasValidas = 0
+    for columnas in range(9):
+        columna = (_carton[0][columnas],_carton[1][columnas],_carton[2][columnas])
+        if(bingo.contarCeldasOcupadas(columna) == 1):
+            columnasValidas+=1
+    
+    assert columnasValidas == 3
+
+def test_menosDe3CeldasPorColumna():
+    assert bingo.menosDe3CeldasPorColumna(_carton) == 0
 
 def test_columnasOcupadas():
     assert bingo.columnasOcupadas(_carton) == 9
-
-def test_filasOcupadas():
-    assert bingo.filasOcupadas(_carton) == 3
 
 def test_celdasEntre1y90():
     assert bingo.valoresValidos(_carton) == 15
@@ -26,3 +41,19 @@ def test_celdaDerecha():
 
 def test_celdaRepetida():
     assert bingo.celdasDiferentes(_carton) == 15
+
+def test_5CeldasPorFila():
+    celdasOcupadas = 0
+    filasValidas = 0
+    for filas in _carton:
+        celdasOcupadas = bingo.contarCeldasOcupadas(filas)
+        if(celdasOcupadas==5):
+            filasValidas+=1
+    
+    assert filasValidas == 3
+
+def test_masDe2CeldasOcupadasConsecutivas():
+    assert bingo.celdasOcupadasConsecutivas(_carton) == 0
+
+def test_masDe2CeldasVaciasConsecutivas():
+    assert bingo.celdasVaciasConsecutivas(_carton) == 0
